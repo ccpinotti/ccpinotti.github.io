780e0af585c526a3b2e9c191e7fc1e24e435b1df
ccpinotti <ccpinotti@gmail.com>
2017-06-30 10:50:40 -0700

    adding lots of things, this is a bad commit message from Jean

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,17 @@
 // sanity check
 console.log("app.js linked.");
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    alert("Welcome to my site")
+});
+function greeting(name) {
+  alert('Hello ' + name);
+}
+
+function processUserInput(callback) {
+  var name = prompt('Please enter your name.');
+  callback(name);
+}
+
+processUserInput(greeting);
